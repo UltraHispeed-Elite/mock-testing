12061c02a1b196968880e776436dba41f4ec5deb
UltraHispeed-Elite <62296749+UltraHispeed-Elite@users.noreply.github.com>
2024-07-13 16:12:44 -0500

    moved map functions

--- js/game.js
+++ js/game.js
@@ -1,7 +1,8 @@
 function gameSetup() {
     let screen = createCanvas(600,600);
     screen.position(0,0);
-    
+
+    mapSetup();
     playerSetup();
 }
 
---
+++ js/game_map.js
@@ -0,0 +1,7 @@
+var game_map;
+
+function mapSetup() {
+    game_map = createMap(9, 9, 16);
+
+    console.log(game_map);
+}
