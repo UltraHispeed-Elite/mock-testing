69434e36156f40299abd22bcf4c69668a0efeebb
UltraHispeed-Elite <62296749+UltraHispeed-Elite@users.noreply.github.com>
2024-07-13 16:38:12 -0500

    found starting point

--- js/game_map.js
+++ js/game_map.js
@@ -1,7 +1,26 @@
 var game_map;
+var starting_point;
 
 function mapSetup() {
     game_map = createMap(9, 9, 16);
-
     console.log(game_map);
+
+    locateStartingMap();
+    console.log(starting_point)
+}
+
+function mapDraw() {
+
+}
+
+function locateStartingMap() {
+    for(let i = 0; i < game_map.length; i++) {
+        for(let j = 0; j < game_map[i].length; j++) {
+            if(game_map[i][j] === 1) {
+                starting_point = [i, j];
+            }else {
+                continue;
+            }
+        }
+    }
 }
